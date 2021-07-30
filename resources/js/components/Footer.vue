1eaf25b07107acefcdf0d4111b853ae4147a922f
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-07-30 17:13:32 +0200

    completato giorno 5

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="text-center">
+        &copy; Alessandro De Santis
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+
+</style>
