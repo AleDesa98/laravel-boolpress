9defa96bfce5a36e5e456e45f50e4d5a8ebac1f1
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-08-02 16:38:01 +0200

    completato giorno 6: Vue Router

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -3,8 +3,10 @@
         <div class="card w-100">
             <div class="card-body">
                 <h4 class="card-title">{{ item.title }}</h4>
+
                 <p>{{ item.excerpt }}</p>
-                <a href="#" class="card-link">Leggi</a>
+                
+                <router-link :to="{ name: 'single-post', params: { slug: item.slug } }">Leggi</router-link>
             </div>
         </div>       
     </div>
---
+++ resources/js/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="my-5">
+        <h1>Chi siamo</h1>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nihil tenetur, eos facilis dolores veritatis voluptate expedita harum magnam similique optio ad maxime maiores corrupti alias id tempore dolore qui aliquam enim ullam odio excepturi! Quas repellendus quibusdam reiciendis eos dolorem corrupti ducimus dolore vero officiis perferendis! Esse, quam dignissimos!</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style>
+
+</style> 
